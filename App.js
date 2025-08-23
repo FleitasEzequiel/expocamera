9278a98affeb5d76236691fb2efe5f2b796116ae
FleitasEzequiel <ezequielafeita@gmail.com>
2025-08-23 20:27:21 -0300

    Camara

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
 
 export default function App() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>Queremos usar tu cámara</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <CameraView style={styles.camera} facing={'front'}>
+          <Text> a </Text>
+        <View style={styles.buttonContainer}>
+          <View style={styles.oval}>
+            <Text style={styles.text}>Poné tu jeta acá </Text>
+          </View>
+          {/* <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            {/* <Text style={styles.text}>Flip Camera</Text> */}
+          {/* </TouchableOpacity> }*/} 
+      <TouchableOpacity style={styles.button}>
+        <Text>Hola</Text>
+      </TouchableOpacity>
+        </View>
+      </CameraView>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  oval:{
+    // backgroundColor:"red",
+    borderColor:"red",
+    borderWidth:3,
+    display:"flex",
+    // width:"30vw 
+    height:"60%",
+    width:"100%",
+    borderRadius:"50%",
+    alignSelf:"center",
+    justifyContent:"center",
+  },
+  camera: {
+    flex: 1,
+    // alignSelf:"center",
+    // borderRadius:150
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    // flex: 1,
+    backgroundColor:"blue",
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf:"center"
   },
 });
